56c887af4a652285f551cdff5c16ff2cdbef1718
thearasengkh <seng.theara.kh@gmail.com>
2017-06-19 18:39:22 +0700

    initial commit

---
+++ src/main/java/com/kshrd/spring/SpringBootThymeleafHomeworkApplication.java
@@ -0,0 +1,12 @@
+package com.kshrd.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafHomeworkApplication.class, args);
+	}
+}
