35e1224ef7b882cf873425c9317fd9087dfdca38
thearasengkh <seng.theara.kh@gmail.com>
2017-06-19 19:30:24 +0700

    add admin template

--- src/main/java/com/kshrd/spring/controller/AdminController.java
+++ src/main/java/com/kshrd/spring/controller/AdminController.java
@@ -1,9 +1,14 @@
 package com.kshrd.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
 
+	@RequestMapping("/admin/dashboard")
+	public String adminDashboard(){
+		return "admin/dashboard";
+	}
 	
 }
