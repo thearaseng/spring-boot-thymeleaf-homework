66c0467b8b89c679c20e0d52f620039daa35c71c
ITE_19 <=seng.theara.kh@gmail.com>
2017-06-21 11:06:26 +0700

    add userCreate controller

--- src/main/java/com/kshrd/spring/controller/AdminController.java
+++ src/main/java/com/kshrd/spring/controller/AdminController.java
@@ -5,8 +5,11 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kshrd.spring.model.User;
 import com.kshrd.spring.service.UserService;
@@ -32,6 +35,12 @@ public class AdminController {
 	public String userCU(Model model){
 		model.addAttribute("USER", new User());
 		return "/admin/user-cu";
+	}
+	
+	@RequestMapping(value="/api/user/create", method=RequestMethod.POST)
+	public ModelAndView userCreate(@ModelAttribute User user, Model model){
+		userService.addUser(user);
+		return new ModelAndView("redirect:/admin/user-list");
 	}
 	
 }
