831f2c54e9e75262afc72672925241c9b638a795
ITE_19 <=seng.theara.kh@gmail.com>
2017-06-21 11:26:50 +0700

    remove auto add user when getting users list

--- src/main/java/com/kshrd/spring/data/Database.java
+++ src/main/java/com/kshrd/spring/data/Database.java
@@ -8,12 +8,6 @@ public class Database {
 	private ArrayList<User> users = new ArrayList<>();
 	
 	public ArrayList<User> getUsers(){
-		User user = new User();
-		user.setId(1);
-		user.setName("Theara");
-		user.setProfile("prisfsdfdf/sdfs/f/sd/fds");
-		user.setRole("admin");
-		users.add(user);
 		return users;
 	}
 	
