e043cb607009787c347cacbdb2c317f9f1c268f9
thearasengkh <seng.theara.kh@gmail.com>
2017-06-22 10:32:51 +0700

    add MyBatis config

---
+++ src/main/java/com/kshrd/spring/configuration/MyBatisConfiguration.java
@@ -0,0 +1,35 @@
+package com.kshrd.spring.configuration;
+
+import javax.sql.DataSource;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+@Configuration
+@MapperScan("com.kshrd.spring.repositories")
+public class MyBatisConfiguration {
+
+	private DataSource dataSource;
+	
+	@Autowired
+	public MyBatisConfiguration(DataSource dataSource){
+		this.dataSource = dataSource;
+	}
+	
+	@Bean
+	public DataSourceTransactionManager transactionManager(){
+		return new DataSourceTransactionManager(dataSource);
+	}
+	
+	@Bean
+	public SqlSessionFactoryBean sqlSessionFactoryBean() throws Exception{
+		SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
+		sessionFactoryBean.setDataSource(dataSource);;
+		return sessionFactoryBean;
+	}
+	
+}
