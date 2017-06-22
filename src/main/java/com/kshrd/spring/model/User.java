9b66b334f76389a2634bf4126a9ac26f28fd9d1f
thearasengkh <seng.theara.kh@gmail.com>
2017-06-22 12:53:43 +0700

    change User class implementation

--- src/main/java/com/kshrd/spring/model/User.java
+++ src/main/java/com/kshrd/spring/model/User.java
@@ -3,40 +3,68 @@ package com.kshrd.spring.model;
 public class User {
 
 	private int id;
-	
-	private String name, profile;
-	private int role;
-
+	private String userName, email, gender, phoneNumber, userHash, password, profileUrl;
+	private int roleId;
+	private boolean status;
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
-	public String getName() {
-		return name;
+	public String getUserName() {
+		return userName;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
-
-	public int getRole() {
-		return role;
+	public String getEmail() {
+		return email;
 	}
-
-	public void setRole(int role) {
-		this.role = role;
+	public void setEmail(String email) {
+		this.email = email;
 	}
-
-	public String getProfile() {
-		return profile;
+	public String getGender() {
+		return gender;
 	}
-
-	public void setProfile(String profile) {
-		this.profile = profile;
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getUserHash() {
+		return userHash;
+	}
+	public void setUserHash(String userHash) {
+		this.userHash = userHash;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getProfileUrl() {
+		return profileUrl;
+	}
+	public void setProfileUrl(String profileUrl) {
+		this.profileUrl = profileUrl;
+	}
+	public int getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
 	}
 	
 }
--- src/main/java/com/kshrd/spring/repository/UserRepository.java
+++ src/main/java/com/kshrd/spring/repository/UserRepository.java
@@ -1,15 +1,29 @@
 package com.kshrd.spring.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import com.kshrd.spring.model.Role;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
 import com.kshrd.spring.model.User;
 
 public interface UserRepository {
 
-	ArrayList<User> getAllUsers();
-	User addUser(User user);
-	List<Role> getRoles();
+	@Select("SELECT "
+			+ "	U.id, "
+			+ "	U.user_name, "
+			+ "	U.email, "
+			+ "	U.password, "
+			+ "	U.gender, "
+			+ "	U.user_hash,"
+			+ " R.role_name"
+			+ " FROM "
+			+ "	users U INNER JOIN role R ON U.role_id = R.id")
+	@Results(value={
+			@Result(property="userHash" , column="user_hash"),
+			@Result(property="role.roleName", column="role_name")
+	})
+	public List<User> getUsers();
 	
 }
